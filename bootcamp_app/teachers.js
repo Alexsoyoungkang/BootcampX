6215184bfcc44f48769d2dc1fc54f53650a64e8b
Soyoung <ksyoung3424@gmail.com>
2023-07-15 00:19:29 +0000

    SQL injection fix

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -15,7 +15,7 @@ const queryString = `
   JOIN assistance_requests ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+  WHERE cohorts.name LIKE $1
   ORDER BY teacher;
 `;
 const values = [`%${cohortName}%`];
